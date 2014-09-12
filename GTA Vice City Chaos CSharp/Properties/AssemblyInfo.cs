7c963474396fead9ddac4088773d12eb2815d26c
Lighnat0r <Lighnat0r@gmail.com>
2014-09-12 17:40:57 +0200

    stuff

--- GTA Vice City Chaos CSharp/Properties/AssemblyInfo.cs
+++ GTA Vice City Chaos CSharp/Properties/AssemblyInfo.cs
@@ -36,4 +36,3 @@ using System.Resources;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 //[assembly: AssemblyInformationalVersion("1.2.1")]
-[assembly: NeutralResourcesLanguageAttribute("en-GB")]
