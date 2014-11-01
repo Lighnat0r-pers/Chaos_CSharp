46201ca9d26f8c3ef1eba7a1da4c49603c366089
Lighnat0r <Lighnat0r@gmail.com>
2014-11-01 17:51:05 +0000

    more stuff

---
+++ GTA Vice City Chaos CSharp/ModsLoop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GTAVC_Chaos
+{
+    static class ModsLoop
+    {
+        /// <summary>
+        /// Main method that should be called continuously. This method is in charge of
+        /// activating the different modules in the mod (e.g. TimedEffects, StaticEffects etc)
+        /// </summary>
+        static public void Update()
+        {
+
+        }
+    }
+}
