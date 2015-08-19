1ea59baea11aebe06024f8f32e2951c5203281b0
Lighnat0r <Lighnat0r@gmail.com>
2015-08-19 13:53:17 +0200

    cleanup getting xml files and add init for MemoryAddresses.xml

--- GTAVC_Chaos/Components.cs
+++ GTAVC_Chaos/Components.cs
@@ -13,56 +13,94 @@ namespace GTAVC_Chaos
         static public TimedEffect[] timedEffects;
         static public PermanentEffect[] permanentEffects;
         static public StaticEffect[] staticEffects;
-        static XmlDocument timedEffectsFile;
+
+        static string baseResourceString     = "GTAVC_Chaos.";
+        static string memoryAddressesFile    = "MemoryAddresses.xml";
+        static string memoryAddressesSchema  = "MemoryAddressSchema.xsd";
+        static string timedEffectsFile       = "TimedEffects.xml";
+        static string timedEffectsSchema     = "TimedEffectSchema.xsd";
+        static string permanentEffectsFile   = "PermanentEffects.xml";
+        static string permanentEffectsSchema = "PermanentEffectSchema.xsd";
+        static string staticEffectsFile      = "StaticEffects.xml";
+        static string staticEffectsSchema    = "StaticEffectSchema.xsd";
+
 
         static public void Init()
         {
-
+            InitMemoryAddresses();
             InitTimedEffects();
             InitPermanentEffects();
             InitStaticEffects();
         }
 
-        static void settingsValidationEventHandler(object sender, ValidationEventArgs e)
+        static void xmlValidationEventHandler(object sender, ValidationEventArgs e)
         {
             if (e.Severity == XmlSeverityType.Warning)
             {
-                Debug.Write("WARNING: ");
-                Debug.WriteLine(e.Message);
+                Debug.WriteLine("WARNING: " + e.Message);
             }
             else
             {
-                Debug.Write("ERROR: ");
-                Debug.WriteLine(e.Message);
+                Debug.WriteLine("ERROR: " + e.Message);
+            }
+        }
+
+        static XmlSchemaSet getXmlSchemaSet(string filename)
+        {
+            XmlSchemaSet schemas = new XmlSchemaSet();
+
+            // NOTE(Ligh): Gets the schema from an external file.
+            //XmlReader x = new XmlTextReader(filename);
+
+            // NOTE(Ligh): Gets the schema from an embedded resource.
+            Stream x = Assembly.GetExecutingAssembly().GetManifestResourceStream(baseResourceString + filename);
+
+            schemas.Add(XmlSchema.Read(x, null));
+
+            return schemas;
+        }
+
+        static XmlDocument getXmlDocument(string xmlFilename, string xsdFilename)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(xmlFilename);
+            document.Schemas = getXmlSchemaSet(xsdFilename);
+            document.Validate(new ValidationEventHandler(xmlValidationEventHandler));
+
+            return document;
+        }
+
+        static void InitMemoryAddresses()
+        {
+            XmlDocument file = getXmlDocument(memoryAddressesFile, memoryAddressesSchema);
+            ReadMemoryAddresses(file);
+        }
+
+        static void ReadMemoryAddresses(XmlDocument file)
+        {
+            XmlNodeList effects = file.SelectNodes("//addresses/memoryaddress");
+            foreach (XmlNode effect in effects)
+            {
+                XmlNode nameNode = effect.SelectSingleNode("name");
+                XmlNode addressNode = effect.SelectSingleNode("address");
+                XmlNode typeNode = effect.SelectSingleNode("Type");
+                XmlNode sizeNode = effect.SelectSingleNode("size");
+                if (sizeNode == null)
+                {
+
+                }
             }
         }
 
         static void InitTimedEffects()
         {
-
-            XmlSchemaSet schemas = new XmlSchemaSet();
-
-            // NOTE(Ligh): Gets the schema from an external file.
-            //XmlReader x = new XmlTextReader("TimedEffectSchema.xsd");
-
-            // NOTE(Ligh): Gets the schema from an embedded resource.
-            Stream x = Assembly.GetExecutingAssembly().GetManifestResourceStream("GTAVC_Chaos.TimedEffectSchema.xsd");
-            
-            XmlSchema schema = XmlSchema.Read(x, null);
-
-            schemas.Add(schema);
-
-            timedEffectsFile = new XmlDocument();
-            timedEffectsFile.Load("TimedEffects.xml");
-            timedEffectsFile.Schemas = schemas;
-            timedEffectsFile.Validate(new ValidationEventHandler(settingsValidationEventHandler));
-            ReadTimedEffects();
-
+            XmlDocument file = getXmlDocument(timedEffectsFile, timedEffectsSchema);
+            ReadTimedEffects(file);
         }
 
-        static void ReadTimedEffects()
+        static void ReadTimedEffects(XmlDocument file)
         {
-            XmlNodeList effects = timedEffectsFile.SelectNodes("//timedeffects/timedeffect");
+            XmlNodeList effects = file.SelectNodes("//timedeffects/timedeffect");
             foreach (XmlNode effect in effects)
             {
                 XmlNode nameNode = effect.SelectSingleNode("name");
@@ -89,10 +127,22 @@ namespace GTAVC_Chaos
 
         static void InitPermanentEffects()
         {
+            XmlDocument file = getXmlDocument(permanentEffectsFile, permanentEffectsSchema);
+            ReadPermanentEffects(file);
+        }
+
+        static void ReadPermanentEffects(XmlDocument file)
+        {
 
         }
 
         static void InitStaticEffects()
+        {
+            XmlDocument file = getXmlDocument(staticEffectsFile, staticEffectsSchema);
+            ReadStaticEffects(file);
+        }
+
+        static void ReadStaticEffects(XmlDocument file)
         {
 
         }
