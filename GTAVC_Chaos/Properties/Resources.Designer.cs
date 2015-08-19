230c8e0c743a6893f9a69e55847c26fc4b7053ce
Lighnat0r <Lighnat0r@gmail.com>
2015-08-19 13:32:02 +0200

    add MemoryAddressSchema as embedded resource

--- GTAVC_Chaos/Properties/Resources.Designer.cs
+++ GTAVC_Chaos/Properties/Resources.Designer.cs
@@ -61,6 +61,29 @@ namespace GTAVC_Chaos.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
+        ///&lt;xs:schema id=&quot;MemoryAddressSchema&quot; elementFormDefault=&quot;qualified&quot; xmlns:xs=&quot;http://www.w3.org/2001/XMLSchema&quot;&gt;
+        ///
+        ///  &lt;xs:element name=&quot;addresses&quot;&gt;
+        ///    &lt;xs:complexType&gt;
+        ///      &lt;xs:sequence&gt;
+        ///        &lt;xs:element name=&quot;memoryaddress&quot;  maxOccurs=&quot;unbounded&quot; type=&quot;MemoryAddress&quot;/&gt;
+        ///      &lt;/xs:sequence&gt;
+        ///    &lt;/xs:complexType&gt;
+        ///  &lt;/xs:element&gt;
+        ///
+        ///  &lt;xs:complexType name=&quot;MemoryAddress&quot;&gt;
+        ///    &lt;xs:sequence&gt;
+        ///      &lt;xs:element name=&quot;name&quot; type=&quot;xs:string&quot;/&gt;
+        ///      &lt;xs:element n [rest of string was truncated]&quot;;.
+        /// </summary>
+        public static string MemoryAddressSchema {
+            get {
+                return ResourceManager.GetString("MemoryAddressSchema", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         public static System.Drawing.Icon SunriseIcon {
