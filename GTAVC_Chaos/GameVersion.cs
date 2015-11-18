c9d6c0fe6d17693105af4230e40b4189eea305ec
Lighnat0r <Lighnat0r@gmail.com>
2015-11-18 23:24:49 +0100

    simplify getting offset for version

--- GTAVC_Chaos/GameVersion.cs
+++ GTAVC_Chaos/GameVersion.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace GTAVC_Chaos
@@ -19,39 +20,7 @@ namespace GTAVC_Chaos
 
         public long GetOffsetForVersion(long address)
         {
-            long key = FindLastKeySmallerThanOrEqualTo<long>(offsets.Keys, address);
-
-            return offsets[key];
-        }
-
-        private static T FindLastKeySmallerThanOrEqualTo<T>(IList<T> sortedCollection, T key) where T : IComparable<T>
-        {
-            int index = FindFirstIndexGreaterThan<T>(sortedCollection, key) - 1;
-
-            if (index < 0)
-            {
-                throw new Exception("Tried to get address below defined range of offsets.");
-            }
-            return sortedCollection[index];
-        }
-
-        private static int FindFirstIndexGreaterThan<T>(IList<T> sortedCollection, T key) where T : IComparable<T>
-        {
-            int begin = 0;
-            int end = sortedCollection.Count;
-            while (end > begin)
-            {
-                int index = (begin + end) / 2;
-                T el = sortedCollection[index];
-                if (el.CompareTo(key) > 0)
-                    end = index;
-                else
-                    begin = index + 1;
-            }
-
-            return end;
+            return offsets.First(kvp => kvp.Key >= address).Value;
         }
     }
-
-
 }
